b0d7f692792952877dbd33789f547ad684f2f482
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2024-01-19 15:18:38 -0300

    feat: add omie offer api call feature

--- app/(user)/offer/[uuid]/page.tsx
+++ app/(user)/offer/[uuid]/page.tsx
@@ -1,9 +1,22 @@
-export default function Example({
+import { fetchOfferById } from "@/app/lib/actions";
+
+export default async function Example({
   params,
 }: {
   params: {
     uuid: string;
   };
 }) {
-  return <div>{params.uuid}</div>;
+  const offer = await (await fetchOfferById(parseInt(params.uuid))).pedido_venda_produto
+
+  return (
+    <div>
+      <ul>
+        <li>Codigo: {offer.cabecalho.codigo_pedido}</li>
+        <li>Valor mercadorias: {offer.total_pedido.valor_mercadorias}</li>
+        <li>Valor total: {offer.total_pedido.valor_total_pedido}</li>
+        <li>Observacoes de venda: {offer.observacoes?.obs_venda}</li>
+      </ul>
+    </div>
+  )
 }
