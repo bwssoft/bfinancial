7b26e10b50c4fab9b03727131a14b1e73f5fa234
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2024-02-15 18:23:17 -0300

    feat: add better crt path check

--- app/lib/bwspay/grpc.ts
+++ app/lib/bwspay/grpc.ts
@@ -3,7 +3,9 @@ import * as protoLoader from '@grpc/proto-loader';
 import path from 'path';
 import { promisify } from 'util';
 import fs from 'fs'
+
 const PROTO_PATH = path.join(process.cwd(), './app/lib/bwspay/payment.proto');
+const CRT_PATH = path.join(process.cwd(), './app/lib/bwspay/ca.crt')
 
 // suggested options for similarity to loading grpc.load behavior
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
@@ -25,7 +27,7 @@ const target = 'bpay-grpc-api.bwsoft.app';
 
 export class BwPay extends TransactionService {
   constructor() {
-    super(target, grpc.credentials.createSsl(fs.readFileSync("C:\\Users\\user\\Documents\\Develop\\Work\\bws-financial\\app\\lib\\bwspay\\ca.crt")));
+    super(target, grpc.credentials.createSsl(fs.readFileSync(CRT_PATH)));
   }
 
   public async creactePixDetached(params: any) {
