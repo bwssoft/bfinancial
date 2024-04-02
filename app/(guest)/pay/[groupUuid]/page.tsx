7eeae4d4d650ec2275b6e9f45c4780fbba2f49f8
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2024-04-02 10:29:14 -0300

    feat: add download button on qrcode references

--- app/(guest)/pay/[groupUuid]/page.tsx
+++ app/(guest)/pay/[groupUuid]/page.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import { fetchPaymentByGroup, getManyTransactionById } from "@/app/lib/actions";
+import { Button } from "@/app/ui/button";
 import { LabelValue } from "@/app/ui/label-value";
 import { Surface, SurfaceFooter, SurfaceHeader } from "@/app/ui/surface";
 import { generateQR } from "@/app/utils/qrCode";
@@ -7,17 +9,18 @@ import { CopyQrCode } from "./copy-qrcode";
 interface PageProps {
   params: {
     groupUuid: string;
-  }
+  };
 }
 
-const pixLogoSrc = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg/2560px-Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg.png"
+const pixLogoSrc =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg/2560px-Logo%E2%80%94pix_powered_by_Banco_Central_%28Brazil%2C_2020%29.svg.png";
 
 export default async function PayPage({ params }: PageProps) {
   const payments = await fetchPaymentByGroup(params.groupUuid);
 
   const { transactions } = await getManyTransactionById({
-    id: payments?.map((pay) => pay.bpay_transaction_id)
-  })
+    id: payments?.map((pay) => pay.bpay_transaction_id),
+  });
 
   const payment = payments?.[0] ?? null;
   const transaction = transactions?.[0] ?? null;
@@ -42,10 +45,22 @@ export default async function PayPage({ params }: PageProps) {
 
             <div className="p-4 text-sm space-y-4">
               <div className="w-full inline-flex items-center justify-center">
-                <img src={qrCodeSrc} className="w-[18.125rem] h-[18.125rem]" alt="QR code pix" />
+                <img
+                  src={qrCodeSrc}
+                  className="w-[18.125rem] h-[18.125rem] border p-2 rounded-sm"
+                  alt="QR code pix"
+                />
               </div>
 
-              <CopyQrCode pixCode={transaction?.bb?.pixCopyPaste} /> 
+              <div className="flex justify-center">
+                <Button asChild>
+                  <a href={qrCodeSrc} download>
+                    Baixar QRCode
+                  </a>
+                </Button>
+              </div>
+
+              <CopyQrCode pixCode={transaction?.bb?.pixCopyPaste} />
 
               <ol className="list-decimal pl-[1.2em]">
                 <li>Acesse o aplicativo do seu banco</li>
@@ -62,16 +77,25 @@ export default async function PayPage({ params }: PageProps) {
             </SurfaceHeader>
 
             <div className="p-4 space-y-4">
-              <LabelValue label="Parcela" value={payment?.omie_metadata.numero_parcela?.toString()} type="inline" />
+              <LabelValue
+                label="Parcela"
+                value={payment?.omie_metadata.numero_parcela?.toString()}
+                type="inline"
+              />
               <LabelValue label="Vencimento" value="22/02/2024" type="inline" />
             </div>
 
             <SurfaceFooter className="p-4">
-              <LabelValue className="text-lg" label="Total" value={`R$${payment?.price.toString()}`} type="inline" />
+              <LabelValue
+                className="text-lg"
+                label="Total"
+                value={`R$${payment?.price.toString()}`}
+                type="inline"
+              />
             </SurfaceFooter>
           </Surface>
         </div>
       </div>
     </main>
-  )
+  );
 }
