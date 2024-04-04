9a32cfea5c8f0838c0a7bae4d8cdeb2e54649186
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2024-04-04 09:33:49 -0300

    feat: add better way to upload file to catch errors

--- app/lib/firebase/firebase.ts
+++ app/lib/firebase/firebase.ts
@@ -1,44 +1,45 @@
-import { env } from '@/app/lib/enviroment'
-import { FirebaseApp, FirebaseOptions, initializeApp } from 'firebase/app';
-import { FirebaseStorage, getBytes, getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
+import { env } from "@/app/lib/enviroment";
+import { FirebaseApp, FirebaseOptions, initializeApp } from "firebase/app";
+import {
+  FirebaseStorage,
+  UploadResult,
+  getBytes,
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytes,
+} from "firebase/storage";
 
 const firebaseConfig: FirebaseOptions = env.FIREBASE_CONFIG;
 
 class FirebaseGateway {
-  private _app: FirebaseApp
-  private _storage: FirebaseStorage
+  private _app: FirebaseApp;
+  private _storage: FirebaseStorage;
 
   constructor() {
-      this._app = initializeApp(firebaseConfig);
-      this._storage = getStorage(this._app);
+    this._app = initializeApp(firebaseConfig);
+    this._storage = getStorage(this._app);
   }
 
-  async uploadFile(input: { buffer: Buffer, name: string, type: string }): Promise<string> {
-      const { buffer, name, type } = input
+  async uploadFile(input: { buffer: Buffer; name: string; type: string }): Promise<string> {
+    const { buffer, name, type } = input;
 
-      const fileRef = ref(this._storage, `b-financial/qr-codes/${name}`);
+    const fileRef = ref(this._storage, `b-financial/qr-codes/${name}`);
 
-      const fileUrl = await uploadBytes(
-          fileRef,
-          buffer,
-          { contentType: type }
-      ).then(
-          async (snapshot) => {
-              const url = await getDownloadURL(snapshot.ref).then((url) => url);
-              return url;
-          }
-      );
+    const fileUrl: UploadResult = await uploadBytes(fileRef, buffer, { contentType: type });
 
-      return fileUrl;
-  };
+    const url = await getDownloadURL(fileUrl.ref).then((url) => url);
+
+    return url;
+  }
 
   async downloadFile(input: { bucket: string }) {
-      const { bucket } = input
-      const fileRef = ref(this._storage, bucket);
-      return await getBytes(fileRef)
+    const { bucket } = input;
+    const fileRef = ref(this._storage, bucket);
+    return await getBytes(fileRef);
   }
 }
 
 const firebaseGateway = new FirebaseGateway();
 
-export {firebaseGateway as FirebaseGateway} 
+export { firebaseGateway as FirebaseGateway };
