198a65f7ececb10918b2b9812184f3df9d63abcb
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2024-04-04 14:42:18 -0300

    feat: change current transaction form action trigger flow to fit with toasts

--- app/(user)/payment/[uuid]/current-transaction.tsx
+++ app/(user)/payment/[uuid]/current-transaction.tsx
@@ -1,20 +1,19 @@
 "use client";
 
-import { cn } from "@/app/utils/cn";
+import { useToast } from "@/app/hook/use-toast";
 import { BPayTransaction } from "@/app/lib/definitions/BPayTransaction";
 import { Button } from "@/app/ui/button";
+import { cn } from "@/app/utils/cn";
 
 interface CurrentTransactionProps {
   transaction: BPayTransaction | null;
   qrCodeUrl: string | undefined;
-  action: () => void;
+  action: (formData: FormData) => Promise<void>;
 }
 
-export function CurrentTransaction({
-  transaction,
-  qrCodeUrl,
-  action,
-}: CurrentTransactionProps) {
+export function CurrentTransaction({ transaction, qrCodeUrl, action }: CurrentTransactionProps) {
+  const { toast } = useToast();
+
   const handlePixCodeCopy = () => {
     if (transaction) {
       navigator.clipboard.writeText(transaction?.bb?.pixCopyPaste);
@@ -22,14 +21,28 @@ export function CurrentTransaction({
     }
   };
 
+  async function handleAction(formData: FormData) {
+    try {
+      await action(formData);
+
+      toast({
+        title: "Sucesso!",
+        description: "Cobrança enviada com sucesso!",
+        variant: "success",
+      });
+    } catch (error: any) {
+      toast({
+        title: "Erro",
+        description: error.message,
+        variant: "error",
+      });
+    }
+  }
+
   if (!transaction || transaction.finish) return null;
 
   return (
-    <div
-      className={cn(
-        "bg-white border shadow-sm sm:overflow-hidden sm:rounded-lg"
-      )}
-    >
+    <div className={cn("bg-white border shadow-sm sm:overflow-hidden sm:rounded-lg")}>
       <div className="divide-y divide-gray-200">
         <div className="p-4">
           <h2 id="notes-title" className="text-lg font-medium text-gray-900">
@@ -46,23 +59,17 @@ export function CurrentTransaction({
         </Button>
       </div>
       <div className="border-t gap-2 inline-flex items-center justify-between p-4 w-full">
-        <Button
-          className="w-full"
-          onClick={handlePixCodeCopy}
-          variant="outline"
-        >
+        <Button className="w-full" onClick={handlePixCodeCopy} variant="outline">
           Copiar Código
         </Button>
-        <form action={action}>
+        <form action={handleAction}>
           <Button className="w-full" type="submit" variant="outline">
             Enviar cobrança
           </Button>
         </form>
       </div>
       <div className="border-t gap-2 inline-flex p-4 w-full">
-        <span className="truncate text-sm">
-          {transaction?.bb?.pixCopyPaste}
-        </span>
+        <span className="truncate text-sm">{transaction?.bb?.pixCopyPaste}</span>
       </div>
     </div>
   );
