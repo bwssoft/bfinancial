6c51392097f21639743c70d819eabb854440fafd
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2024-04-05 08:42:57 -0300

    feat: add more logs

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,103 +1,102 @@
-import { OmieEnterpriseEnum } from "@/app/lib/definitions/OmieApi"
-import { paymentRepo } from "@/app/lib/mongodb/repositories/payment.mongo"
-import { OmieOrderService } from "@/app/lib/omie/order.omie"
-import { OmieReceiveOrdersService } from "@/app/lib/omie/receive-orders.omie"
-import axios from "axios"
+import { OmieEnterpriseEnum } from "@/app/lib/definitions/OmieApi";
+import { paymentRepo } from "@/app/lib/mongodb/repositories/payment.mongo";
+import { OmieOrderService } from "@/app/lib/omie/order.omie";
+import { OmieReceiveOrdersService } from "@/app/lib/omie/receive-orders.omie";
+import axios from "axios";
 
 export async function POST(request: Request) {
   /**
    * data received from bb webhook
-  */
-  const data: { pix: Pix[] } = await request.json()
-  const [pix] = data.pix
+   */
+  const data: { pix: Pix[] } = await request.json();
+  const [pix] = data.pix;
 
   /**
    * request payment entity associated with txid from bb pix in our repository
-  */
-  const payment = await paymentRepo.findOne({ "bpay_metadata.txid": pix.txid })
-  if (!payment) return new Response("No payment with this code", { status: 404 })
+   */
+  const payment = await paymentRepo.findOne({ "bpay_metadata.txid": pix.txid });
+  if (!payment) return new Response("No payment with this code", { status: 404 });
+  console.log("1 ~ file: route.ts:18 ~ POST ~ payment:", payment);
 
   /**
-  * set omie secrets on service with enterprise associated with payment entity
-  */
-  const { omie_metadata } = payment
+   * set omie secrets on service with enterprise associated with payment entity
+   */
+  const { omie_metadata } = payment;
   OmieOrderService.setSecrets(omie_metadata.enterprise);
   OmieReceiveOrdersService.setSecrets(omie_metadata.enterprise);
 
   /**
-  * request omie offer (pedido)
-  */
-  const offer = await OmieOrderService.find(Number(omie_metadata.codigo_pedido))
-  if (!offer) return new Response("No offer with this code", { status: 404 })
+   * request omie offer (pedido)
+   */
+  const offer = await OmieOrderService.find(Number(omie_metadata.codigo_pedido));
+  console.log("2 ~ file: route.ts:32 ~ POST ~ offer:", offer);
+  if (!offer) return new Response("No offer with this code", { status: 404 });
 
   /**
-  * create object to update prop "situação" from omie offer
-  */
-  let _offer = offer.pedido_venda_produto
+   * create object to update prop "situação" from omie offer
+   */
+  let _offer = offer.pedido_venda_produto;
 
-  delete _offer.cabecalho.numero_pedido
-  delete _offer.cabecalho.bloqueado
-  delete _offer.frete
-  delete _offer.informacoes_adicionais
-  delete _offer.exportacao
-  delete _offer.det
+  delete _offer.cabecalho.numero_pedido;
+  delete _offer.cabecalho.bloqueado;
+  delete _offer.frete;
+  delete _offer.informacoes_adicionais;
+  delete _offer.exportacao;
+  delete _offer.det;
 
-  _offer.cabecalho.origem_pedido = "API"
-  const parcela = _offer.lista_parcelas.parcela.map(parcela => {
+  _offer.cabecalho.origem_pedido = "API";
+  const parcela = _offer.lista_parcelas.parcela.map((parcela) => {
     if (parcela.numero_parcela === omie_metadata.numero_parcela) {
       return {
         ...parcela,
         parcela_adiantamento: "S",
         categoria_adiantamento: "1.01.01",
-        conta_corrente_adiantamento: nCodCCByEnterprise[
-          OmieEnterpriseEnum[omie_metadata.enterprise]
-        ],
-      }
+        conta_corrente_adiantamento:
+          nCodCCByEnterprise[OmieEnterpriseEnum[omie_metadata.enterprise]],
+      };
     }
-    return parcela
-  })
-  _offer.lista_parcelas.parcela = parcela
-
-
-  /**
-  * update omie offer (pedido) 
-  */
-  await OmieOrderService.update(_offer)
+    return parcela;
+  });
+  console.log("3 ~ file: route.ts:61 ~ parcela ~ parcela:", parcela);
+  _offer.lista_parcelas.parcela = parcela;
 
   /**
-  * request for all omie receive orders (contas a receber)
-  */
+   * update omie offer (pedido)
+   */
+  await OmieOrderService.update(_offer);
+  console.log("4 ~ file: route.ts:69 OmieOrderService.update");
+  /**
+   * request for all omie receive orders (contas a receber)
+   */
   const receive_orders = await OmieReceiveOrdersService.findAll({
     pagina: 1,
     registros_por_pagina: 1000,
-    filtrar_cliente: Number(omie_metadata.codigo_cliente)
-  })
+    filtrar_cliente: Number(omie_metadata.codigo_cliente),
+  });
+  console.log("5 ~ file: route.ts:78 ~ POST ~ receive_orders:", receive_orders);
 
-  if (!receive_orders) return new Response(
-    "No receive offer with this code",
-    {
-      status: 404
-    }
-  )
-
-  /**
-  * filter omie receive orders that fit with payment entity
-  */
-  const [current_receive_order] = receive_orders.conta_receber_cadastro.filter(a =>
-    Number(a.nCodPedido) === Number(omie_metadata.codigo_pedido) &&
-    Number(a.numero_parcela.split('/')[0]) === Number(omie_metadata.numero_parcela)
-  )
-
-  if (!current_receive_order) return new Response(
-    "No receive offer linked",
-    {
-      status: 404
-    }
-  )
+  if (!receive_orders)
+    return new Response("No receive offer with this code", {
+      status: 404,
+    });
 
   /**
-  * 
-  */
+   * filter omie receive orders that fit with payment entity
+   */
+  const [current_receive_order] = receive_orders.conta_receber_cadastro.filter(
+    (a) =>
+      Number(a.nCodPedido) === Number(omie_metadata.codigo_pedido) &&
+      Number(a.numero_parcela.split("/")[0]) === Number(omie_metadata.numero_parcela)
+  );
+
+  if (!current_receive_order)
+    return new Response("No receive offer linked", {
+      status: 404,
+    });
+
+  /**
+   *
+   */
   await OmieReceiveOrdersService.post({
     codigo_lancamento: current_receive_order.codigo_lancamento_omie,
     codigo_conta_corrente: current_receive_order.id_conta_corrente,
@@ -105,44 +104,44 @@ export async function POST(request: Request) {
     data: new Date().toLocaleDateString("pt-BR", {
       day: "2-digit",
       month: "2-digit",
-      year: "numeric"
+      year: "numeric",
     }),
-    observacao: "Baixa realizada automaticamente pelo sistema BFinancial"
-  })
+    observacao: "Baixa realizada automaticamente pelo sistema BFinancial",
+  });
+  console.log("6 ~ file: route.ts:78 ~ POST");
 
   /**
-  *  notify bpay microservice with the dara received from bb webhook
-  */
-  await axios.post(
-    "https://bpay-rest-api.bwsoft.app/update-pix-without-recipient",
-    data
-  )
+   *  notify bpay microservice with the dara received from bb webhook
+   */
+  await axios.post("https://bpay-rest-api.bwsoft.app/update-pix-without-recipient", data);
 
-  return Response.json({ ok: true })
+  console.log("7 ~ file: route.ts:78 ~ AXIOS POST");
+
+  return Response.json({ ok: true });
 }
 
 const nCodCCByEnterprise = {
-  WFC: '7974650357', // descrição: adiantamento
-  ICB: '6331754348', // descrição: adiantamento
-  ICBFILIAL: '8397776986', //codigo_banco 999 //nao é possivel por que não esta com o cnpj da priscila vinculado e a aplicação no bb não está em produção
-  MGC: '8402307862', //'2225386193', //codigo_banco 999
-  BWS: '2225316057'//codigo_banco 999
-}
+  WFC: "7974650357", // descrição: adiantamento
+  ICB: "6331754348", // descrição: adiantamento
+  ICBFILIAL: "8397776986", //codigo_banco 999 //nao é possivel por que não esta com o cnpj da priscila vinculado e a aplicação no bb não está em produção
+  MGC: "8402307862", //'2225386193', //codigo_banco 999
+  BWS: "2225316057", //codigo_banco 999
+};
 
 interface Pix {
-  endToEndId: string
-  txid: string
-  valor: string
+  endToEndId: string;
+  txid: string;
+  valor: string;
   componentesValor: {
     original: {
-      valor: string
-    }
-  }
-  chave: string
-  horario: string
-  infoPagador: string
+      valor: string;
+    };
+  };
+  chave: string;
+  horario: string;
+  infoPagador: string;
   pagador: {
-    cpf: string
-    nome: string
-  }
+    cpf: string;
+    nome: string;
+  };
 }
