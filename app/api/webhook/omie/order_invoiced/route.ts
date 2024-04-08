33c33bb624765ce356194f79c1b8071143cc69cc
devitalo97 <dev.italo.souza@gmail.com>
2024-04-08 09:29:23 -0300

    feat: omie webhook

---
+++ app/api/webhook/omie/order_invoiced/route.ts
@@ -0,0 +1,7 @@
+export async function POST(request: Request) {
+  const data = await request.json()
+  console.log("======== data from omie ========")
+  console.log(data)
+  console.log("======== data from omie ========")
+  return Response.json({ ok: true })
+}
