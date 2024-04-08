5896fa6b4758ce7df01ae1a703907034b213189b
devitalo97 <dev.italo.souza@gmail.com>
2024-04-08 14:25:52 -0300

    feat: add props on template email

--- app/ui/email-templates/qrcode-pix/qrcode-pix-template.tsx
+++ app/ui/email-templates/qrcode-pix/qrcode-pix-template.tsx
@@ -1,8 +1,20 @@
-import { Body, Column, Container, Head, Html, Row, Section, Text } from "@react-email/components";
+import {
+  Body,
+  Column,
+  Container,
+  Head,
+  Html,
+  Img,
+  Row,
+  Section,
+  Text,
+} from "@react-email/components";
 import { Tailwind } from "@react-email/tailwind";
 import { Heading } from "./components/heading";
 
-export default function QRCodeEmail() {
+export default function QRCodeEmail(params: {
+  qrCode: { code: string; url: string }[];
+}) {
   return (
     <Html>
       <Head />
@@ -33,32 +45,36 @@ export default function QRCodeEmail() {
 
             <Section>
               <Heading as="h2">Pix</Heading>
-              <Section
-                align="center"
-                className="border border-solid border-gray-300 bg-gray-100 rounded-md p-4"
-              >
-                <Text className="text-center">
-                  Você pode utilizar a câmera do seu celular para ler o QR Code pix
-                </Text>
-
-                <Section className="h-64 w-64 rounded-md border border-solid border-gray-300 bg-gray-50" />
-
-                <Text className="text-center">
-                  Ou copiar o código e pagar no aplicativo do seu banco
-                </Text>
-
-                <Section className="bg-white w-full rounded-lg p-4 border border-solid border-gray-200">
-                  <Text className="m-0 text-xs">
-                    00020101021226830014br.gov.bcb.pix2561api.pagseguro.com/pix/v2/8ABED4B4-5090-41D6-B86D-CB87FA9824A727600016BR.COM.PAGSEGURO01368ABED4B4-5090-41D6-B86D-CB87FA9824A7520489995303986540512.305802BR5922PAGSEGURO
-                    TECNOLOGIA L6009Sao Paulo62070503***630446AE
+              {params.qrCode.map(({ url, code }, index) => (
+                <Section
+                  key={index}
+                  align="center"
+                  className="border border-solid border-gray-300 bg-gray-100 rounded-md p-4"
+                >
+                  <Text className="text-center">
+                    Você pode utilizar a câmera do seu celular para ler o QR
+                    Code pix
                   </Text>
+
+                  <Section className="h-64 w-64 rounded-md border border-solid border-gray-300 bg-gray-50">
+                    <Img src={url} className="border p-2 rounded-sm" />
+                  </Section>
+
+                  <Text className="text-center">
+                    Ou copiar o código e pagar no aplicativo do seu banco
+                  </Text>
+
+                  <Section className="bg-white w-full rounded-lg p-4 border border-solid border-gray-200">
+                    <Text className="m-0 text-xs">{code}</Text>
+                  </Section>
                 </Section>
-              </Section>
+              ))}
             </Section>
 
             <Section className="border border-solid mt-10 text-center border-transparent border-t-gray-300">
               <Text className="text-gray-500">
-                Este e-mail não responde mensagens. E-mail enviado de forma automática.
+                Este e-mail não responde mensagens. E-mail enviado de forma
+                automática.
               </Text>
             </Section>
           </Container>
