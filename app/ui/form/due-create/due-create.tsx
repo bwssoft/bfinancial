773f74ec653240e7b7608a20d68ed4fd3f120cb1
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2024-04-08 14:30:23 -0300

    feat: add due create form

---
+++ app/ui/form/due-create/due-create.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { OmieClientModel } from "@/app/lib/definitions/OmieClient";
+import { Alert } from "@/app/ui/alert";
+import { Button } from "@/app/ui/button";
+import { FormGroup } from "@/app/ui/form-group";
+import { Input } from "@/app/ui/input";
+import { Label } from "@/app/ui/label";
+
+export interface DueCreateFormProps {
+  client: OmieClientModel;
+  action: (formData: FormData) => void;
+}
+
+export function DueCreateForm({ client, action }: DueCreateFormProps) {
+  return (
+    <form action={action} className="flex flex-col gap-4">
+      <Alert
+        title="Dados de contato OMIE"
+        subtitle="Enviaremos o QR code do pix para os contatos abaixo. Caso estejam desatualizados, edite-os antes de enviar."
+      />
+
+      <FormGroup>
+        <Label>Email</Label>
+        <Input name="contact_email" defaultValue={client?.email} />
+      </FormGroup>
+      <FormGroup>
+        <Label>Telefone</Label>
+        <Input
+          name="contact_phone"
+          defaultValue={
+            client?.telefone1_ddd
+              ? `${client?.telefone1_ddd}${client?.telefone1_numero}`
+              : undefined
+          }
+        />
+      </FormGroup>
+
+      <Button type="submit" size="lg" className="w-full">
+        Efetuar cobrança
+      </Button>
+    </form>
+  );
+}
