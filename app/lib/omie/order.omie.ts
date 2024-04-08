83078a6b15c417ce3a755aa86d07f110439c010d
devitalo97 <dev.italo.souza@gmail.com>
2024-04-08 15:24:21 -0300

    feat(lib): add method on omie order service

--- app/lib/omie/order.omie.ts
+++ app/lib/omie/order.omie.ts
@@ -5,51 +5,62 @@ import {
   OmieListOfferResponse,
   OmieOffer,
   OmieOfferUpdateResponse,
+  OmieInvoice,
+  OmieInvoiceResponse
 } from "../definitions/OmieOffer";
 import { OmieBaseService } from "./base.omie";
 
 class OrderService extends OmieBaseService {
   async find(codigo_pedido: number) {
-      const data = this.formatSingleBodyRequest("ConsultarPedido", {
-        codigo_pedido,
-      });
-      const response = await this._httpProvider.post<OmieFindOfferResponse>(
-        "/produtos/pedido/",
-        data
-      );
-      return response.data;
+    const data = this.formatSingleBodyRequest("ConsultarPedido", {
+      codigo_pedido,
+    });
+    const response = await this._httpProvider.post<OmieFindOfferResponse>(
+      "/produtos/pedido/",
+      data
+    );
+    return response.data;
   }
 
   async findAll(params?: OmieSearchParams) {
-      const data = this.formatBody("ListarPedidos", params);
-      const response = await this._httpProvider.post<OmieListOfferResponse>(
-        "/produtos/pedido/",
-        data
+    const data = this.formatBody("ListarPedidos", params);
+    const response = await this._httpProvider.post<OmieListOfferResponse>(
+      "/produtos/pedido/",
+      data
+    );
+    response.data.pedido_venda_produto.sort((a, b) => {
+      const first = parse(
+        `${a.infoCadastro.dInc}-${a.infoCadastro.hInc}`,
+        "dd/MM/yyyy-HH:mm:ss",
+        new Date()
       );
-      response.data.pedido_venda_produto.sort((a, b) => {
-        const first = parse(
-          `${a.infoCadastro.dInc}-${a.infoCadastro.hInc}`,
-          "dd/MM/yyyy-HH:mm:ss",
-          new Date()
-        );
-        const second = parse(
-          `${b.infoCadastro.dInc}-${b.infoCadastro.hInc}`,
-          "dd/MM/yyyy-HH:mm:ss",
-          new Date()
-        );
+      const second = parse(
+        `${b.infoCadastro.dInc}-${b.infoCadastro.hInc}`,
+        "dd/MM/yyyy-HH:mm:ss",
+        new Date()
+      );
 
-        return differenceInSeconds(second, first);
-      });
-      return response.data;
+      return differenceInSeconds(second, first);
+    });
+    return response.data;
   }
 
   async update(params: OmieOffer) {
-      const data = this.formatBody("AlterarPedidoVenda", params);
-      const response = await this._httpProvider.post<OmieOfferUpdateResponse>(
-        "/produtos/pedido/",
-        data
-      );
-      return response.data;
+    const data = this.formatBody("AlterarPedidoVenda", params);
+    const response = await this._httpProvider.post<OmieOfferUpdateResponse>(
+      "/produtos/pedido/",
+      data
+    );
+    return response.data;
+  }
+
+  async invoice(params: OmieInvoice) {
+    const data = this.formatBody("FaturarPedidoVenda", params);
+    const response = await this._httpProvider.post<OmieInvoiceResponse>(
+      "/produtos/pedidovendafat/",
+      data
+    );
+    return response.data;
   }
 }
 
