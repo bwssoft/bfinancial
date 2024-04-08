101ee1f298c86ffee004546353894259be0601d6
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2024-04-08 15:17:57 -0300

    feat: add more info on qrcode pix template

--- app/ui/email-templates/qrcode-pix/qrcode-pix-template.tsx
+++ app/ui/email-templates/qrcode-pix/qrcode-pix-template.tsx
@@ -12,9 +12,24 @@ import {
 import { Tailwind } from "@react-email/tailwind";
 import { Heading } from "./components/heading";
 
-export default function QRCodeEmail(params: {
-  qrCode: { code: string; url: string }[];
-}) {
+interface QRCodeEmailProps {
+  metadata: {
+    clientName: string;
+    total: number;
+    createdAt: string;
+  };
+  installments: {
+    code: string;
+    url: string;
+    data_vencimento: string;
+    valor: number;
+    numero_parcela: number;
+  }[];
+}
+
+export default function QRCodeEmail(props: QRCodeEmailProps) {
+  const { metadata, installments } = props;
+
   return (
     <Html>
       <Head />
@@ -23,7 +38,7 @@ export default function QRCodeEmail(params: {
           <Container>
             <Heading>Nova cobrança BWS</Heading>
             <Text className="text-lg text-gray-500 leading-7 [&:not(:first-child)]:mt-6">
-              Olá, meu clientezinho!
+              Olá, {metadata.clientName}!
             </Text>
 
             <Section>
@@ -33,11 +48,11 @@ export default function QRCodeEmail(params: {
                 <Row>
                   <Column>
                     <p className="m-0 text-gray-400">Data</p>
-                    <span>21/02/2024</span>
+                    <span>{metadata.createdAt}</span>
                   </Column>
                   <Column>
                     <p className="m-0 text-gray-400">Valor</p>
-                    <span>R$19.00</span>
+                    <span>R${metadata.total}</span>
                   </Column>
                 </Row>
               </Section>
@@ -45,27 +60,53 @@ export default function QRCodeEmail(params: {
 
             <Section>
               <Heading as="h2">Pix</Heading>
-              {params.qrCode.map(({ url, code }, index) => (
-                <Section
-                  key={index}
-                  align="center"
-                  className="border border-solid border-gray-300 bg-gray-100 rounded-md p-4"
-                >
-                  <Text className="text-center">
-                    Você pode utilizar a câmera do seu celular para ler o QR
-                    Code pix
-                  </Text>
 
-                  <Section className="h-64 w-64 rounded-md border border-solid border-gray-300 bg-gray-50">
-                    <Img src={url} className="border p-2 rounded-sm" />
-                  </Section>
+              {installments.map((installment, index) => (
+                <Section key={index}>
+                  <Heading as="h3">
+                    Parcela {installment.numero_parcela} de {installments.length}
+                  </Heading>
 
-                  <Text className="text-center">
-                    Ou copiar o código e pagar no aplicativo do seu banco
-                  </Text>
+                  <Section
+                    align="center"
+                    className="border border-solid border-gray-200 bg-gray-100 rounded-md mt-2 p-4"
+                  >
+                    <table className="w-full table-fixed">
+                      <tbody>
+                        <tr>
+                          <td>
+                            <Column className="w-full border bg-white mr-1 justify-center border-solid rounded-md h-24 border-gray-300">
+                              <Text className="font-semibold text-center m-0">
+                                Data de vencimento
+                              </Text>
+                              <Text className="text-center m-0">{installment.data_vencimento}</Text>
+                            </Column>
+                          </td>
+                          <td>
+                            <Column className="w-full border bg-white ml-1 justify-center border-solid rounded-md h-24 border-gray-300">
+                              <Text className="font-semibold text-center m-0">Valor</Text>
+                              <Text className="text-center m-0">R${installment.valor}</Text>
+                            </Column>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
 
-                  <Section className="bg-white w-full rounded-lg p-4 border border-solid border-gray-200">
-                    <Text className="m-0 text-xs">{code}</Text>
+                    <Text className="text-center">
+                      Você pode utilizar a câmera do seu celular para ler o QR Code pix
+                    </Text>
+
+                    <Section className="h-64 w-64 rounded-md border border-solid border-gray-200 bg-gray-50">
+                      <Img src={installment.url} />
+                    </Section>
+
+                    <Text className="text-center">
+                      Ou copiar o código e pagar no aplicativo do seu banco
+                    </Text>
+
+                    <Section className="bg-white w-full rounded-lg p-4 border border-solid border-gray-200">
+                      <Text className="m-0 text-xs">{installment.code}</Text>
+                    </Section>
                   </Section>
                 </Section>
               ))}
@@ -73,8 +114,7 @@ export default function QRCodeEmail(params: {
 
             <Section className="border border-solid mt-10 text-center border-transparent border-t-gray-300">
               <Text className="text-gray-500">
-                Este e-mail não responde mensagens. E-mail enviado de forma
-                automática.
+                Este e-mail não responde mensagens. E-mail enviado de forma automática.
               </Text>
             </Section>
           </Container>
