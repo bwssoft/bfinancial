1a62da7e7ba6e8bc7f5ff3ae67f45f91c9dfb9d8
devitalo97 <dev.italo.souza@gmail.com>
2024-04-12 11:17:24 -0300

    feat(page): inserts due detached form component on payment page and modal

--- app/(user)/@modal/(.)payment/create/page.tsx
+++ app/(user)/@modal/(.)payment/create/page.tsx
@@ -1,5 +1,16 @@
+import { OmieEnterpriseEnum } from "@/app/lib/definitions/OmieApi";
+import { DueDetachedForm } from "@/app/ui/form/due-detached";
 import { Modal } from "@/app/ui/modal";
 
-export default function Example() {
-  return null;
+export default function Example({
+  searchParams,
+}: {
+  searchParams: { omie_enterprise: keyof typeof OmieEnterpriseEnum };
+}) {
+  const { omie_enterprise } = searchParams;
+  return (
+    <Modal title="Cobrar Frete">
+      <DueDetachedForm omie_enterprise={OmieEnterpriseEnum[omie_enterprise]} />
+    </Modal>
+  );
 }
--- app/(user)/payment/create/page.tsx
+++ app/(user)/payment/create/page.tsx
@@ -1,3 +1,13 @@
-export default function Example() {
-  return <div>Payment Create</div>;
+import { OmieEnterpriseEnum } from "@/app/lib/definitions/OmieApi";
+import { DueDetachedForm } from "@/app/ui/form/due-detached";
+
+export default function Example({
+  searchParams,
+}: {
+  searchParams: { omie_enterprise: keyof typeof OmieEnterpriseEnum };
+}) {
+  const { omie_enterprise } = searchParams;
+  return (
+    <DueDetachedForm omie_enterprise={OmieEnterpriseEnum[omie_enterprise]} />
+  );
 }
