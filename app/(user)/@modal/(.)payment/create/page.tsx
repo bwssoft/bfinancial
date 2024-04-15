113b16627dd418e8e78665d14f4ea97e103306d1
devitalo97 <dev.italo.souza@gmail.com>
2024-04-15 12:03:37 -0300

    feat(page): implement 'due-detached' form on pages

--- app/(user)/@modal/(.)payment/create/page.tsx
+++ app/(user)/@modal/(.)payment/create/page.tsx
@@ -1,16 +1,51 @@
+import { getCachedClient, getCachedOffer } from "@/app/lib/actions";
 import { OmieEnterpriseEnum } from "@/app/lib/definitions/OmieApi";
 import { DueDetachedForm } from "@/app/ui/form/due-detached";
 import { Modal } from "@/app/ui/modal";
 
-export default function Example({
+export default async function Example({
   searchParams,
 }: {
-  searchParams: { omie_enterprise: keyof typeof OmieEnterpriseEnum };
+  searchParams: {
+    omie_enterprise: keyof typeof OmieEnterpriseEnum;
+    codigo_pedido_omie: string;
+    codigo_cliente_omie: string;
+  };
 }) {
-  const { omie_enterprise } = searchParams;
+  const { omie_enterprise, codigo_cliente_omie, codigo_pedido_omie } =
+    searchParams;
+
+  if (!omie_enterprise || !codigo_cliente_omie || !codigo_pedido_omie) {
+    throw new Error("Lmao");
+  }
+
+  const [offer, client] = await Promise.all([
+    getCachedOffer(
+      OmieEnterpriseEnum[omie_enterprise],
+      Number(codigo_pedido_omie)
+    ),
+    getCachedClient(OmieEnterpriseEnum[omie_enterprise], codigo_cliente_omie),
+  ]);
+
+  if (!offer || !client) {
+    return (
+      <Modal title="Cobrar Frete">
+        <div>Faltam dados</div>
+      </Modal>
+    );
+  }
+
   return (
     <Modal title="Cobrar Frete">
-      <DueDetachedForm omie_enterprise={OmieEnterpriseEnum[omie_enterprise]} />
+      <DueDetachedForm
+        contact_phone={`${client.telefone1_ddd}${client.telefone1_numero}`}
+        contact_email={client.email}
+        omie_enterprise={OmieEnterpriseEnum[omie_enterprise]}
+        cnpj_cpf={client.cnpj_cpf}
+        codigo_cliente_omie={codigo_cliente_omie}
+        codigo_pedido_omie={codigo_pedido_omie}
+        nome_fantasia={client.nome_fantasia}
+      />
     </Modal>
   );
 }
