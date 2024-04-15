cc5c3ecb769c515a4a0458f3722c357d49e8a52d
devitalo97 <dev.italo.souza@gmail.com>
2024-04-15 12:02:25 -0300

    feat(ui): create form to charge detached pix

--- app/ui/form/due-detached/index.tsx
+++ app/ui/form/due-detached/index.tsx
@@ -1,12 +1,60 @@
+"use client";
 import { OmieEnterpriseEnum } from "@/app/lib/definitions/OmieApi";
 import { Button } from "../../button";
+import { EnvelopeIcon } from "@heroicons/react/20/solid";
+import { createDetachedPaymentFromOfferPage } from "@/app/lib/actions";
+import { toast } from "@/app/hook/use-toast";
 
 export function DueDetachedForm(props: {
   omie_enterprise?: OmieEnterpriseEnum;
+  contact_email: string;
+  contact_phone: string;
+  codigo_pedido_omie: string;
+  cnpj_cpf: string;
+  codigo_cliente_omie: string;
+  nome_fantasia: string;
 }) {
-  const { omie_enterprise } = props;
+  const {
+    omie_enterprise,
+    contact_email,
+    contact_phone,
+    codigo_pedido_omie,
+    cnpj_cpf,
+    codigo_cliente_omie,
+    nome_fantasia,
+  } = props;
+
+  async function handleAction(form: FormData) {
+    try {
+      const createPaymentFromOfferPageBinded =
+        createDetachedPaymentFromOfferPage.bind(null, {
+          codigo_pedido_omie: codigo_pedido_omie,
+          cnpj_cpf: cnpj_cpf,
+          codigo_cliente_omie: codigo_cliente_omie,
+          nome_fantasia: nome_fantasia,
+        });
+
+      await createPaymentFromOfferPageBinded(form);
+
+      toast({
+        title: "Sucesso!",
+        description: "Nova cobrança efetuada com sucesso!",
+        variant: "success",
+      });
+    } catch (error: any) {
+      toast({
+        title: "Erro",
+        description: error.message,
+        variant: "error",
+      });
+    }
+  }
+
   return (
-    <form className="space-y-8 overflow-y-auto max-h-full scroll-slim p-2 border-4 border-transparent py-4">
+    <form
+      action={handleAction}
+      className="space-y-8 overflow-y-auto max-h-full scroll-slim p-2 border-4 border-transparent py-4"
+    >
       <section className="space-y-4">
         <fieldset>
           <label
@@ -18,7 +66,7 @@ export function DueDetachedForm(props: {
           <select
             id="omie_enterprise"
             name="omie_enterprise"
-            className="mt-1 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="mt-1 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-cyan-600 sm:text-sm sm:leading-6"
             defaultValue={omie_enterprise}
           >
             <option value="default">Selecione uma empresa</option>
@@ -33,38 +81,10 @@ export function DueDetachedForm(props: {
         <fieldset>
           <div>
             <label
-              htmlFor="price"
+              htmlFor="contact_phone"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Valor
-            </label>
-            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
-              <div className="pointer-events-none inset-y-0 left-0 flex items-center pl-3">
-                <span className="text-gray-500 sm:text-sm">R$</span>
-              </div>
-              <input
-                type="text"
-                name="price"
-                autoComplete="price"
-                className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                placeholder="0.00"
-              />
-              <div className="pointer-events-none inset-y-0 right-0 flex items-center pr-3">
-                <span className="text-gray-500 sm:text-sm" id="price-currency">
-                  BRL
-                </span>
-              </div>
-            </div>
-          </div>
-        </fieldset>
-
-        <fieldset>
-          <div>
-            <label
-              htmlFor="phone-number"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Telefone para envio da cobrança
+              Telefone do cliente
             </label>
             <div className="relative mt-2 rounded-md shadow-sm">
               <div className="absolute inset-y-0 left-0 flex items-center">
@@ -75,18 +95,75 @@ export function DueDetachedForm(props: {
                   id="country"
                   name="country"
                   autoComplete="country"
-                  className="h-full rounded-md border-0 bg-transparent py-0 pl-3 pr-7 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                  className="h-full rounded-md border-0 bg-transparent py-0 pl-3 pr-7 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm"
                 >
                   <option selected={true}>BR</option>
                 </select>
               </div>
               <input
                 type="text"
-                name="phone-number"
-                id="phone-number"
-                className="block w-full rounded-md border-0 py-1.5 pl-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                name="contact_phone"
+                id="contact_phone"
+                defaultValue={contact_phone}
+                className="block w-full rounded-md border-0 py-1.5 pl-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
                 placeholder="(11) 9 9954-6543"
               />
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <div>
+            <label
+              htmlFor="contact_email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email do cliente
+            </label>
+            <div className="relative mt-2 rounded-md shadow-sm">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                <EnvelopeIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </div>
+              <input
+                type="email"
+                name="contact_email"
+                id="contact_email"
+                defaultValue={contact_email}
+                className="block w-full rounded-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+                placeholder="you@example.com"
+              />
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <div>
+            <label
+              htmlFor="price"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Valor
+            </label>
+            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-cyan-600">
+              <div className="pointer-events-none inset-y-0 left-0 flex items-center pl-3">
+                <span className="text-gray-500 sm:text-sm">R$</span>
+              </div>
+              <input
+                type="text"
+                name="price"
+                id="price"
+                autoComplete="price"
+                className="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                placeholder="0.00"
+              />
+              <div className="pointer-events-none inset-y-0 right-0 flex items-center pr-3">
+                <span className="text-gray-500 sm:text-sm" id="price-currency">
+                  BRL
+                </span>
+              </div>
             </div>
           </div>
         </fieldset>
