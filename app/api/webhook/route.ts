ac7798b9d66694c4b1c926ce3b48011aa8031534
devitalo97 <dev.italo.souza@gmail.com>
2024-04-17 16:35:44 -0300

    feat(api): check if payment is detached

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -18,103 +18,104 @@ export async function POST(request: Request) {
   */
   const payment = await paymentRepo.findOne({ "bpay_metadata.txid": pix.txid })
   if (!payment) return new Response("No payment with this code", { status: 500, statusText: "No payment with this code" })
-
-  /**
-  * set omie secrets on service with enterprise associated with payment entity
-  */
-  const { omie_metadata } = payment
-  OmieOrderService.setSecrets(omie_metadata.enterprise);
-  OmieReceiveOrdersService.setSecrets(omie_metadata.enterprise);
-
-  /**
-  * request omie offer (pedido)
-  */
-  const offer = await getCachedOffer(omie_metadata.enterprise, parseInt(omie_metadata.codigo_pedido));
-  if (!offer) return new Response("No offer with this code", { status: 500, statusText: "No offer with this code" })
-
-  if (offer.pedido_venda_produto.cabecalho.etapa !== "60") {
+  if (!payment?.is_detached) {
     /**
-    * create object to update prop "situação" from omie offer
+    * set omie secrets on service with enterprise associated with payment entity
     */
-    let _offer = offer.pedido_venda_produto
+    const { omie_metadata } = payment
+    OmieOrderService.setSecrets(omie_metadata.enterprise);
+    OmieReceiveOrdersService.setSecrets(omie_metadata.enterprise);
 
-    delete _offer.cabecalho.numero_pedido
-    delete _offer.cabecalho.bloqueado
-    delete _offer.frete
-    delete _offer.informacoes_adicionais
-    delete _offer.exportacao
-    delete _offer.det
+    /**
+    * request omie offer (pedido)
+    */
+    const offer = await getCachedOffer(omie_metadata.enterprise, parseInt(omie_metadata.codigo_pedido));
+    if (!offer) return new Response("No offer with this code", { status: 500, statusText: "No offer with this code" })
 
-    _offer.cabecalho.origem_pedido = "API"
-    const parcela = _offer.lista_parcelas.parcela.map(parcela => {
-      if (parcela.numero_parcela === omie_metadata.numero_parcela) {
-        return {
-          ...parcela,
-          parcela_adiantamento: "S",
-          categoria_adiantamento: "1.01.01",
-          conta_corrente_adiantamento: nCodCCByEnterprise[
-            OmieEnterpriseEnum[omie_metadata.enterprise]
-          ],
+    if (offer.pedido_venda_produto.cabecalho.etapa !== "60") {
+      /**
+      * create object to update prop "situação" from omie offer
+      */
+      let _offer = offer.pedido_venda_produto
+
+      delete _offer.cabecalho.numero_pedido
+      delete _offer.cabecalho.bloqueado
+      delete _offer.frete
+      delete _offer.informacoes_adicionais
+      delete _offer.exportacao
+      delete _offer.det
+
+      _offer.cabecalho.origem_pedido = "API"
+      const parcela = _offer.lista_parcelas.parcela.map(parcela => {
+        if (parcela.numero_parcela === omie_metadata.numero_parcela) {
+          return {
+            ...parcela,
+            parcela_adiantamento: "S",
+            categoria_adiantamento: "1.01.01",
+            conta_corrente_adiantamento: nCodCCByEnterprise[
+              OmieEnterpriseEnum[omie_metadata.enterprise]
+            ],
+          }
         }
-      }
-      return parcela
-    })
-    _offer.lista_parcelas.parcela = parcela
+        return parcela
+      })
+      _offer.lista_parcelas.parcela = parcela
+
+      /**
+      * update omie offer (pedido) 
+      */
+      await OmieOrderService.update(_offer)
+    }
 
     /**
-    * update omie offer (pedido) 
+    * request for all omie receive orders (contas a receber)
     */
-    await OmieOrderService.update(_offer)
-  }
-
-  /**
-  * request for all omie receive orders (contas a receber)
-  */
-  const receive_orders = await OmieReceiveOrdersService.findAll({
-    pagina: 1,
-    registros_por_pagina: 1000,
-    filtrar_cliente: Number(omie_metadata.codigo_cliente)
-  })
-
-  if (!receive_orders) return new Response(
-    "No receive offer with this code",
-    {
-      status: 500,
-      statusText: "No receive offer with this code"
-    }
-  )
-
-  /**
-  * filter omie receive orders that fit with payment entity
-  */
-  const [current_receive_order] = receive_orders.conta_receber_cadastro.filter(a =>
-    Number(a.nCodPedido) === Number(omie_metadata.codigo_pedido) &&
-    Number(a.numero_parcela.split('/')[0]) === Number(omie_metadata.numero_parcela)
-  )
-
-  if (!current_receive_order) return new Response(
-    "No receive offer linked",
-    {
-      status: 500,
-      statusText: "No receive offer linked"
-    }
-  )
-
-  /**
-  * check omie receive order status is different of "RECEBIDO" and then update the omie receive order
-  */
-  if (current_receive_order.status_titulo !== "RECEBIDO") {
-    await OmieReceiveOrdersService.post({
-      codigo_lancamento: current_receive_order.codigo_lancamento_omie,
-      codigo_conta_corrente: current_receive_order.id_conta_corrente,
-      valor: payment.price,
-      data: new Date().toLocaleDateString("pt-BR", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric"
-      }),
-      observacao: "Baixa realizada automaticamente pelo sistema BFinancial"
+    const receive_orders = await OmieReceiveOrdersService.findAll({
+      pagina: 1,
+      registros_por_pagina: 1000,
+      filtrar_cliente: Number(omie_metadata.codigo_cliente)
     })
+
+    if (!receive_orders) return new Response(
+      "No receive offer with this code",
+      {
+        status: 500,
+        statusText: "No receive offer with this code"
+      }
+    )
+
+    /**
+    * filter omie receive orders that fit with payment entity
+    */
+    const [current_receive_order] = receive_orders.conta_receber_cadastro.filter(a =>
+      Number(a.nCodPedido) === Number(omie_metadata.codigo_pedido) &&
+      Number(a.numero_parcela.split('/')[0]) === Number(omie_metadata.numero_parcela)
+    )
+
+    if (!current_receive_order) return new Response(
+      "No receive offer linked",
+      {
+        status: 500,
+        statusText: "No receive offer linked"
+      }
+    )
+
+    /**
+    * check omie receive order status is different of "RECEBIDO" and then update the omie receive order
+    */
+    if (current_receive_order.status_titulo !== "RECEBIDO") {
+      await OmieReceiveOrdersService.post({
+        codigo_lancamento: current_receive_order.codigo_lancamento_omie,
+        codigo_conta_corrente: current_receive_order.id_conta_corrente,
+        valor: payment.price,
+        data: new Date().toLocaleDateString("pt-BR", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric"
+        }),
+        observacao: "Baixa realizada automaticamente pelo sistema BFinancial"
+      })
+    }
   }
 
   /**
