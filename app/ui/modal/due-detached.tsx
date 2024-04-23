963de98d3a89647acb7fd70ae6cf6c37018da9a1
devitalo97 <dev.italo.souza@gmail.com>
2024-04-23 11:32:42 -0300

    feat(ui): create client component to handle interaction with modal

---
+++ app/ui/modal/due-detached.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { OmieClientModel } from "@/app/lib/definitions/OmieClient";
+import { DueDetachedForm } from "../form/due-detached";
+import { Modal } from "../modal";
+import { OmieEnterpriseEnum } from "@/app/lib/definitions/OmieApi";
+import { useSearchParams } from "next/navigation";
+
+export function DueDetachedModal(params: {
+  client: OmieClientModel;
+  omie_enterprise: OmieEnterpriseEnum;
+  codigo_cliente_omie: string;
+  codigo_pedido_omie: string;
+}) {
+  const { client, omie_enterprise, codigo_cliente_omie, codigo_pedido_omie } =
+    params;
+
+  const searchParams = useSearchParams();
+  const isOpen = searchParams.get("modalIsOpen") === "true" ? true : false;
+
+  const closeModal = () => {
+    return `/offer/${omie_enterprise}/${codigo_cliente_omie}/${codigo_pedido_omie}?modalIsOpen=false`;
+  };
+
+  return isOpen ? (
+    <Modal title="Cobrar Frete" onClose={closeModal}>
+      <DueDetachedForm
+        contact_phone={`${client.telefone1_ddd}${client.telefone1_numero}`}
+        contact_email={client.email}
+        omie_enterprise={omie_enterprise}
+        cnpj_cpf={client.cnpj_cpf}
+        codigo_cliente_omie={codigo_cliente_omie}
+        codigo_pedido_omie={codigo_pedido_omie}
+        nome_fantasia={client.nome_fantasia}
+      />
+    </Modal>
+  ) : (
+    <></>
+  );
+}
